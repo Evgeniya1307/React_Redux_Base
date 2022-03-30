b3d5b28225e83d2358295bc37a883bfd2df4ce0b
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-03-30 15:33:27 +0400

    inpyt text повесили обработчик событий скрытый ввод onChange={handleInput}

--- src/SingleComment.js
+++ src/SingleComment.js
@@ -1,13 +1,13 @@
 import {useState} from "react";
 
 function SingleComment (props){
-    const [textComment, setTextComment] = useState("";)
+    const [textComment, setTextComment] = useState("");
     console.log("swingle comment props>>>>", props);
     
     return (
           <form className="comments-item">
               <div className="comments-item-delete">&times;</div>
-              <input type="text"/>
+              <input type="text" value={textComment}onChange={handleInput}/>
               <input type="sumbit" hidden/>
               </form>    
     )
