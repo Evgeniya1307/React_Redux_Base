d48748433ca010dec42ba5690e10657385b0763c
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-03-30 21:51:34 +0400

    добавила map и key

--- src/Comments.js
+++ src/Comments.js
@@ -1,13 +1,41 @@
+import { useState } from "react";
+import { commentCreate } from "./redux/action";
 import SingleComment from "./SingleComment";
+import uniqid from "uniqid";
+import {useDispatch, useSelector} from "react-redux";
+
 
 function Comments (props){
-    console.log("comments props>>>>", props);
+    const [textComment, setTextComment] = useState("");
+    const comments = useSelector(state=>{
+    const {commentsReducer} = state;
+    return commentsReducer.comments;
+    });
+    const dispatch = useDispatch();
+    
+    
+    const handleInput=(e)=>{
+    setTextComment(e.target.value);
+    }
+
+    const handleSumbit=(e)=>{
+        e.preventDefault();
+        console.log("sumbit textComment>>>",textComment)
+    const  id = uniqid();
+    dispatch(commentCreate(textComment,id));
+    }
+
+    
+    
     return (
         <div className="card-comments">
           <form className="comments-item-create">
-              <input type="text"/>
+          <input type="text" value={textComment}onChange={handleInput}/>
               <input type="sumbit" hidden/>
               </form>  
+              {comments.length &&comments.map(res=>{
+                  return <SingleComment key ={res.id} data={res}/>
+              })}
               <SingleComment/>
         </div>
     )
--- src/SingleComment.js
+++ src/SingleComment.js
@@ -1,13 +1,12 @@
-import {useState} from "react";
-
 function SingleComment (props){
-    const [textComment, setTextComment] = useState("");
-    console.log("swingle comment props>>>>", props);
+  console.log("comment props>>>>", props);
     
+
+
     return (
           <form className="comments-item">
               <div className="comments-item-delete">&times;</div>
-              <input type="text" value={textComment}onChange={handleInput}/>
+              <input type="text"/>
               <input type="sumbit" hidden/>
               </form>    
     )
