65e67f717ac80553ea523a3ce03ae31b4da80e7f
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-03-30 12:05:18 +0400

    сделала switch case increment для увеличение лайков

--- src/redux/stor.js
+++ src/redux/stor.js
@@ -8,7 +8,17 @@ const initialState = {
 
 const reducer = (state=initialState,action)=> {
     console.log("reducer >", action);
-    return state;
+
+switch(action.type){
+    case "INCREMENT":
+        return{
+            ...state,
+            likes:state.likes+1
+        }
+        default:
+            return state;
+}
+
 }
 
 
