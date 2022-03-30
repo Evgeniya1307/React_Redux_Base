a565a54661ba07b15ecd62cb304c287be240d0a0
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-03-30 12:43:41 +0400

    в index.js  сделала import createStore,rootReducer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from "redux";
+import { rootReducer } from './redux/rootReducer';
+
+const store= createStore(rootReducer)
 
 ReactDOM.render(
   <App />,
