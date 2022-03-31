cd81647b3c66f1a7b216bf955595bd1d1b9439a1
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-03-31 13:18:54 +0400

    сделали проверку на true false

--- src/redux/appReducer.js
+++ src/redux/appReducer.js
@@ -8,6 +8,19 @@ const initialState={
 
 export const appReducer=(state=initialState,action)=>{
     switch(action.type){
-        
-    }
-}
+        case LOADER_DISPLAY_ON:
+            return{
+                ...state,
+                loading:true
+            }
+                    case LOADER_DISPLAY_OFF:
+                        return{
+                            ...state,
+                            loading:false
+                        }
+                        default:
+                            return state;
+                    }
+                }
+
+ 
