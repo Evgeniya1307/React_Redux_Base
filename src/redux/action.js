9ebd290bf756782ae3865150420fdeff1d2ab08b
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-03-31 13:34:50 +0400

    задиспатсили loaderOff loaderOn

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,4 +1,4 @@
-import { INCREMENT, DECREMENT, INPUT_TEXT, COMMENT_CREATE,  COMMENT_UPDATE, COMMENT_DELETE, COMMENTS_LOAD } from "./types";
+import { INCREMENT, DECREMENT, INPUT_TEXT, COMMENT_CREATE,  COMMENT_UPDATE, COMMENT_DELETE, COMMENTS_LOAD, LOADER_DISPLAY_ON, LOADER_DISPLAY_OFF  } from "./types";
 
 
 export  function incrementLikes() {
@@ -41,13 +41,32 @@ export  function commentDelete(id) {
     }
 }
 
+export  function loaderOn() {
+    return {
+        type: LOADER_DISPLAY_ON
+    }
+}
+
+
+export  function loaderOff() {
+    return {
+        type: LOADER_DISPLAY_OFF
+    }
+}
+
+
+
+
+
 export  function сommentsLoad() {
     return async dispatch => {
-        const response =fetch("https://jsonplaceholder.typicode.com/comments?_limit=10");    
-    const jsonData=await (await response).json();
+        dispatch (loaderOn());
+        const response = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10");    
+    const jsonData= await response.json();
     dispatch({
         type:COMMENTS_LOAD,
         data:jsonData
-    })
+    });
+    dispatch (loaderOff());
     }
 }
