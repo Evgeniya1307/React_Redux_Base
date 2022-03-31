4f537db17bf95e4d2c19f2bd7c255e797263ecb5
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-03-31 13:46:00 +0400

    сщздали типп error , в appreducer error:null

--- src/redux/action.js
+++ src/redux/action.js
@@ -63,10 +63,13 @@ export  function сommentsLoad() {
         dispatch (loaderOn());
         const response = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10");    
     const jsonData= await response.json();
-    dispatch({
-        type:COMMENTS_LOAD,
-        data:jsonData
-    });
-    dispatch (loaderOff());
+    
+    setTimeout(()=>{
+        dispatch({
+            type:COMMENTS_LOAD,
+            data:jsonData
+        });
+        dispatch (loaderOff());
+    }, 1000) 
     }
 }
--- src/redux/appReducer.js
+++ src/redux/appReducer.js
@@ -2,7 +2,8 @@ import { LOADER_DISPLAY_ON, LOADER_DISPLAY_OFF } from "./types";
 
 
 const initialState={
-    loading:"false"
+    loading:"false",
+    error:null
 }
 
 
